35ceb45730e83ccedb09d94ca27e5f42587d4f91
Frosslee <578146@student.belgiumcampus.ac.za>
2021-10-02 11:44:34 +0200

    login

--- Project_1_PRG/frmLogin.Designer.cs
+++ Project_1_PRG/frmLogin.Designer.cs
@@ -29,12 +29,104 @@ namespace Project_1_PRG
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lbUsername = new System.Windows.Forms.Label();
+            this.lbPassword = new System.Windows.Forms.Label();
+            this.lnkRegister = new System.Windows.Forms.LinkLabel();
+            this.lnkForgottenPassword = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(74, 144);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(100, 20);
+            this.tbPassword.TabIndex = 0;
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Location = new System.Drawing.Point(74, 76);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(100, 20);
+            this.tbUsername.TabIndex = 1;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(85, 200);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // lbUsername
+            // 
+            this.lbUsername.AutoSize = true;
+            this.lbUsername.Location = new System.Drawing.Point(71, 51);
+            this.lbUsername.Name = "lbUsername";
+            this.lbUsername.Size = new System.Drawing.Size(55, 13);
+            this.lbUsername.TabIndex = 4;
+            this.lbUsername.Text = "Username";
+            // 
+            // lbPassword
+            // 
+            this.lbPassword.AutoSize = true;
+            this.lbPassword.Location = new System.Drawing.Point(71, 114);
+            this.lbPassword.Name = "lbPassword";
+            this.lbPassword.Size = new System.Drawing.Size(53, 13);
+            this.lbPassword.TabIndex = 5;
+            this.lbPassword.Text = "Password";
+            // 
+            // lnkRegister
+            // 
+            this.lnkRegister.AutoSize = true;
+            this.lnkRegister.Location = new System.Drawing.Point(95, 226);
+            this.lnkRegister.Name = "lnkRegister";
+            this.lnkRegister.Size = new System.Drawing.Size(46, 13);
+            this.lnkRegister.TabIndex = 6;
+            this.lnkRegister.TabStop = true;
+            this.lnkRegister.Text = "Register";
+            // 
+            // lnkForgottenPassword
+            // 
+            this.lnkForgottenPassword.AutoSize = true;
+            this.lnkForgottenPassword.Location = new System.Drawing.Point(119, 167);
+            this.lnkForgottenPassword.Name = "lnkForgottenPassword";
+            this.lnkForgottenPassword.Size = new System.Drawing.Size(86, 13);
+            this.lnkForgottenPassword.TabIndex = 7;
+            this.lnkForgottenPassword.TabStop = true;
+            this.lnkForgottenPassword.Text = "Forgot Password";
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(258, 283);
+            this.Controls.Add(this.lnkForgottenPassword);
+            this.Controls.Add(this.lnkRegister);
+            this.Controls.Add(this.lbPassword);
+            this.Controls.Add(this.lbUsername);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.tbUsername);
+            this.Controls.Add(this.tbPassword);
+            this.Name = "frmLogin";
             this.Text = "frmLogin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lbUsername;
+        private System.Windows.Forms.Label lbPassword;
+        private System.Windows.Forms.LinkLabel lnkRegister;
+        private System.Windows.Forms.LinkLabel lnkForgottenPassword;
     }
 }
